4717887c8306451f03f10fea508c3c5d6bf9c8dd
Hasdai Pacheco <haxdai@gmail.com>
2015-11-10 20:54:53 -0600

    Configuración inicial y boilerplate de app

---
+++ app/scripts/main.js
@@ -0,0 +1,26 @@
+(function(window) {
+	var app = window.app || {},
+		document = window.document;
+
+	app.version = "0.1.0";
+
+	/*script from https://css-tricks.com/inline-svg-grunticon-fallback*/
+	app.svgSupport = function() {
+	 	var div = document.createElement('div');
+	 	div.innerHTML = '<svg/>';
+		return (div.firstChild && div.firstChild.namespaceURI) == 'http://www.w3.org/2000/svg';
+	};
+
+ 	if (app.svgSupport()) {
+ 		var ajax = new XMLHttpRequest();
+		ajax.open("GET", "images/svg-defs.svg", true);
+		ajax.responseType = "document";
+		ajax.onload = function(e) {
+		  document.body.insertBefore(ajax.responseXML.documentElement,
+		                             document.body.childNodes[0]);
+		}
+		ajax.send();
+ 	}
+
+	window.app = app;
+})(window);
